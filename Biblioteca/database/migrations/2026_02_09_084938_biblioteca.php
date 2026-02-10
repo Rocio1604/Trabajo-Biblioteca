059d38ae0f85da1cf9a4fccbc9e340a2b2c522a8
Ruth Carbajal <ruth.carbajal@juanxxiii.net>
2026-02-10 19:19:55 +0100

    Corrección de tipos de datos y relaciones en migraciones

--- Biblioteca/database/migrations/2026_02_09_084938_biblioteca.php
+++ Biblioteca/database/migrations/2026_02_09_084938_biblioteca.php
@@ -18,7 +18,13 @@ return new class extends Migration
             $table->string('direccion');
             $table->string('telefono');
             $table->string('correo');
+            $table->boolean('activo');
             $table->timestamps();
+        });
+
+        Schema::create('estados_cuotas', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('nombre');
         });
 
         //socios
@@ -29,11 +35,17 @@ return new class extends Migration
             $table->unsignedBigInteger('biblioteca_id');
             $table->string('email');
             $table->string('telefono');
+            $table->unsignedBigInteger('estado_cuota');
             $table->timestamps();
 
             $table->foreign('biblioteca_id')
                   ->references('id')
                   ->on('bibliotecas')
+                  ->onDelete('cascade');
+
+            $table->foreign('estado_cuota')
+                  ->references('id')
+                  ->on('estados_cuotas')
                   ->onDelete('cascade');
         });
         //autores
@@ -48,61 +60,105 @@ return new class extends Migration
         //categorias
         Schema::create('categorias',function(Blueprint $table){
             $table->id();
-            $table->string('categoria');
-            $table->timestamps();
+            $table->string('nombre');
+        });
+
+        //estado de libros
+        Schema::create('estados_libros',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
 
         });
+
+        //disponibilidad libros
+        Schema::create('disponibilidades_libros',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+
+        });
+
         //libros
         Schema::create('libros',function(Blueprint $table){
             $table->id();
-            $table->string('isbn');
+            $table->string('isbn')->unique();
             $table->string('titulo');
-            $table->string('titulo');
-            $table->string('imagen')->nullable();
-            $table->unsignedBigInteger('autor_id');
             $table->unsignedBigInteger('categoria_id');
-            $table->unsignedBigInteger('biblioteca_id');
+            $table->decimal('precio', 8, 2);
             $table->timestamps();
 
-            
             $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
-            $table->foreign('biblioteca_id')->references('id')->on('biblioteca')->onDelete('cascade');
-            
-
         });
         //tabla intermedia libros-autores
-        Schema::create('libros_autores',function(Blueprint $table){
-            $table->id();
+        Schema::create('autor_libro',function(Blueprint $table){
             $table->unsignedBigInteger('autor_id');
             $table->unsignedBigInteger('libro_id');
-            $table->timestamps();
 
             $table->foreign('autor_id')->references('id')->on('autores')->onDelete('cascade');
             $table->foreign('libro_id')->references('id')->on('libros')->onDelete('cascade');
-            
+        });
+
+        //estado de prestamos
+        Schema::create('estados_prestamos',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
 
         });
-        //tabla intermedia libros-categoria
-        Schema::create('libros_categoria',function(Blueprint $table){
+
+        //estado de recibos
+        Schema::create('estados_recibos',function(Blueprint $table){
             $table->id();
-            $table->unsignedBigInteger('categoria_id');
+            $table->string('nombre');
+
+        });
+
+        //tipo de recibos
+        Schema::create('tipos_recibos',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+
+        });
+
+        //ejemplares
+        Schema::create('ejemplares', function (Blueprint $table) {
+            $table->id('id');
             $table->unsignedBigInteger('libro_id');
+            $table->unsignedBigInteger('biblioteca_id');
+            $table->unsignedBigInteger('estado_id');
+            $table->unsignedBigInteger('disponibilidad_id');
+            $table->boolean('es_activo');
             $table->timestamps();
 
-            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
-            $table->foreign('libro_id')->references('id')->on('libros')->onDelete('cascade');
-            
+            $table->foreign('libro_id')
+                  ->references('id')
+                  ->on('libros')
+                  ->onDelete('cascade');
 
+            $table->foreign('biblioteca_id')
+                ->references('id')
+                ->on('bibliotecas')
+                ->onDelete('cascade');
+
+            $table->foreign('estado_id')
+                ->references('id')
+                ->on('estados_libros')
+                ->onDelete('cascade');
+
+            $table->foreign('disponibilidad_id')
+                ->references('id')
+                ->on('disponibilidades_libros')
+                ->onDelete('cascade');
         });
 
         //prestamos
         Schema::create('prestamos', function (Blueprint $table) {
             $table->id('id');
             $table->unsignedBigInteger('socio_id');
-            $table->unsignedBigInteger('libro_id');
+            $table->unsignedBigInteger('ejemplar_id');
+            $table->unsignedBigInteger('biblioteca_id');
             $table->date('fecha_prestamo');
-            $table->date('fecha_limite');
-            $table->date('fecha_devolucion')->nullable();
+            $table->date('fecha_devolucion');
+            $table->decimal('multa', 8, 2)->default(0);
+            $table->unsignedBigInteger('estado_id');
             $table->timestamps();
 
             $table->foreign('socio_id')
@@ -110,46 +166,77 @@ return new class extends Migration
                   ->on('socios')
                   ->onDelete('cascade');
 
-            $table->foreign('libro_id')
+            $table->foreign('ejemplar_id')
                   ->references('id')
-                  ->on('libros')
+                  ->on('ejemplares')
+                  ->onDelete('cascade');
+
+            $table->foreign('estado_id')
+                  ->references('id')
+                  ->on('estados_prestamos')
                   ->onDelete('cascade');
         });
+
         //recibos
         Schema::create('recibos', function (Blueprint $table) {
             $table->id('id');
             $table->unsignedBigInteger('socio_id');
             $table->string('concepto');
-            $table->string('tipo');
+            $table->unsignedBigInteger('tipo_id');
             $table->decimal('importe', 8, 2);
             $table->date('fecha');
+            $table->unsignedBigInteger('estado_id');
             $table->timestamps();
 
             $table->foreign('socio_id')
                   ->references('id')
                   ->on('socios')
                   ->onDelete('cascade');
+
+            $table->foreign('tipo_id')
+                ->references('id')
+                ->on('tipos_recibos')
+                ->onDelete('cascade');
+
+            $table->foreign('estado_id')
+                ->references('id')
+                ->on('estados_recibos')
+                ->onDelete('cascade');
         });
+
+        //roles
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('nombre');
+        });
+
         //usuarios
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id('id');
             $table->string('nombre');
             $table->string('correo')->unique();
             $table->string('telefono');
-            $table->string('rol');
-            $table->string('biblioteca');
-            $table->string('estado');
+            $table->unsignedBigInteger('rol_id');
+            $table->unsignedBigInteger('biblioteca_id');
+            $table->boolean('es_activo');
             $table->timestamps();
+
+            $table->foreign('rol_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+
+             $table->foreign('biblioteca_id')
+                ->references('id')
+                ->on('bibliotecas')
+                ->onDelete('cascade');
         });
         // coches
-        Schema::create('coches', function (Blueprint $table) {
-            $table->unsignedBigInteger('id_usuario');
-            $table->string('pass');
-            $table->timestamps();
+        Schema::create('validaciones_sistema', function (Blueprint $table) {
+            $table->unsignedBigInteger('referencia_id');
+            $table->string('firma_digital');
 
-            $table->primary('id_usuario');
-
-            $table->foreign('id_usuario')
+            $table->foreign('referencia_id')
                   ->references('id')
                   ->on('usuarios')
                   ->onDelete('cascade');
@@ -162,6 +249,23 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('validaciones_sistema');
+        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('ejemplares');
+        Schema::dropIfExists('recibos');
+        Schema::dropIfExists('prestamos');
+        Schema::dropIfExists('tipos_recibos');
+        Schema::dropIfExists('estados_recibos');
+        Schema::dropIfExists('estados_prestamos');
+        Schema::dropIfExists('autor_libro');
+        Schema::dropIfExists('libros');
+        Schema::dropIfExists('disponibilidades_libros');
+        Schema::dropIfExists('estados_libros');
+        Schema::dropIfExists('categorias');
+        Schema::dropIfExists('autores');
+        Schema::dropIfExists('socios');
+        Schema::dropIfExists('estados_cuotas');
+        Schema::dropIfExists('bibliotecas');
     }
 };
